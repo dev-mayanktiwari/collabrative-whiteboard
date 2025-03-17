baebae095b2d3a49b8baa10d529646d9245888f9
Mayank <mayanktiwari1212.mt@gmail.com>
2025-03-17 14:11:30 +0530

    fix: Dashboard and login pages

--- apps/client/src/pages/Login.tsx
+++ apps/client/src/pages/Login.tsx
@@ -1,6 +1,20 @@
+import { authState } from "@repo/store";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useRecoilValue } from "recoil";
 import LoginPage from "~/components/login/LoginForm";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const { user } = useRecoilValue(authState);
+  console.log("Calling from inside pages/Login.tsx", user);
+  
+  useEffect(() => {
+    if (user) {
+      navigate("/dashboard");
+    }
+  }, [user, navigate]);
+
   return <LoginPage />;
 };
 

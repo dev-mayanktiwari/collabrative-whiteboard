c72d4a47c1c691041701469e5eb699453096b280
Mayank <mayanktiwari1212.mt@gmail.com>
2025-03-21 21:35:13 +0530

    feat: Updated layout

--- apps/client/src/components/providers/Layout.tsx
+++ apps/client/src/components/providers/Layout.tsx
@@ -10,7 +10,7 @@ const Layout = ({ children }: LayoutProps) => {
   return (
     <div className="flex flex-col min-h-screen">
       {/* Navbar at the top */}
-      <NavBar areButtonNeeded={true} />
+      <NavBar />
 
       {/* Main content (grows to fill space) */}
       <main className="flex-grow">{children}</main>

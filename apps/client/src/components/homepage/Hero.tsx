555e51caed625834e2a1d5c68a3907818f669fe1
Mayank <mayanktiwari1212.mt@gmail.com>
2025-03-21 21:34:53 +0530

    fix: Some shitty tailwind classes

--- apps/client/src/components/homepage/Hero.tsx
+++ apps/client/src/components/homepage/Hero.tsx
@@ -5,7 +5,7 @@ import CanvasPreview from "./CanvasPreview";
 const Hero = () => {
   return (
     <div className="bg-bg mx-auto pt-16 pb-24 z-10">
-      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between gap-8 z-10">
+      <div className="container mx-auto flex  md:flex-row items-center justify-between gap-8 z-10">
         <div className="max-w-xl">
           <h2 className="text-4xl sm:text-5xl md:text-6xl font-black logo-text leading-tight mb-5">
             <span className="bg-blueCustom px-2 py-2 inline-block rotate-[-1deg] transform -skew-x-1">
@@ -29,7 +29,9 @@ const Hero = () => {
           </Button>
         </div>
         <div className="flex-1 w-full md:w-auto mt-8 md:mt-0">
-          <CanvasPreview />
+          <div className="bg-black">
+            <CanvasPreview />
+          </div>
         </div>
       </div>
     </div>

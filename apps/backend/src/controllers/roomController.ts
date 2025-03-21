9c505bceee4f0a557b78e41ff6b7333270003195
Mayank <mayanktiwari1212.mt@gmail.com>
2025-03-21 21:31:24 +0530

    feat: Added room router

---
+++ apps/backend/src/controllers/roomController.ts
@@ -0,0 +1,223 @@
+import { NextFunction, Request, Response } from "express";
+import { asyncErrorHandler, httpError, httpResponse } from "@repo/shared-utils";
+import {
+  AuthenticatedRequest,
+  CreateRoomInput,
+  GetRoomShapesInput,
+  RenameRoomInput,
+  ResponseMessage,
+  StatusCodes,
+  UpdateRoomShapesInput,
+} from "@repo/types";
+import {
+  createRoom,
+  deleteRoom,
+  getRoomById,
+  getRooms,
+  getRoomShapes,
+  renameRoom,
+  updateRoomShapes,
+} from "@/services/roomDbServices";
+
+export default {
+  createRoom: asyncErrorHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      const { body, user } = req as AuthenticatedRequest;
+
+      const safeParse = CreateRoomInput.safeParse(body);
+      if (!safeParse.success) {
+        return httpError(
+          next,
+          new Error(ResponseMessage.INVALID_INPUT),
+          req,
+          StatusCodes.ERROR.CLIENT_ERROR.BAD_REQUEST
+        );
+      }
+
+      const { name } = safeParse.data;
+
+      const room = await createRoom({
+        roomName: name,
+        userId: String(user?.userId),
+      });
+
+      return httpResponse(
+        req,
+        res,
+        StatusCodes.SUCCESS.CREATED,
+        "Room Created",
+        {
+          room: room,
+        }
+      );
+    }
+  ),
+
+  getRooms: asyncErrorHandler(async (req: Request, res: Response) => {
+    const { user } = req as AuthenticatedRequest;
+
+    const rooms = await getRooms({ userId: String(user?.userId) });
+
+    return httpResponse(req, res, StatusCodes.SUCCESS.OK, "Rooms Fetched", {
+      rooms: rooms,
+    });
+  }),
+
+  getRoomShapes: asyncErrorHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      const { params, user } = req as AuthenticatedRequest;
+
+      const safeParse = GetRoomShapesInput.safeParse(params);
+      if (!safeParse.success) {
+        return httpError(
+          next,
+          new Error(ResponseMessage.INVALID_INPUT),
+          req,
+          StatusCodes.ERROR.CLIENT_ERROR.BAD_REQUEST
+        );
+      }
+
+      const { boardId } = safeParse.data;
+
+      const shapes = await getRoomShapes({
+        roomId: boardId,
+        userId: String(user?.userId),
+      });
+
+      return httpResponse(req, res, StatusCodes.SUCCESS.OK, "Room Fetched", {
+        shapes: shapes,
+      });
+    }
+  ),
+
+  deleteRoom: asyncErrorHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      const { params, user } = req as AuthenticatedRequest;
+
+      const safeParse = GetRoomShapesInput.safeParse(params);
+      if (!safeParse.success) {
+        return httpError(
+          next,
+          new Error(ResponseMessage.INVALID_INPUT),
+          req,
+          StatusCodes.ERROR.CLIENT_ERROR.BAD_REQUEST
+        );
+      }
+
+      const { boardId } = safeParse.data;
+
+      const shapes = await deleteRoom({
+        roomId: boardId,
+        userId: String(user?.userId),
+      });
+
+      return httpResponse(req, res, StatusCodes.SUCCESS.OK, "Room Fetched", {
+        shapes: shapes,
+      });
+    }
+  ),
+
+  renameRoom: asyncErrorHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      const { body, user } = req as AuthenticatedRequest;
+
+      const safeParse = RenameRoomInput.safeParse(body);
+      if (!safeParse.success) {
+        return httpError(
+          next,
+          new Error(ResponseMessage.INVALID_INPUT),
+          req,
+          StatusCodes.ERROR.CLIENT_ERROR.BAD_REQUEST
+        );
+      }
+
+      const { newName, roomId } = safeParse.data;
+
+      const room = await renameRoom({
+        newName: newName,
+        roomId: roomId,
+        userId: String(user?.userId),
+      });
+
+      return httpResponse(
+        req,
+        res,
+        StatusCodes.SUCCESS.CREATED,
+        "Room Updated",
+        {
+          room: room,
+        }
+      );
+    }
+  ),
+
+  updateRoomShapes: asyncErrorHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      const { body, user } = req as AuthenticatedRequest;
+
+      const safeParse = UpdateRoomShapesInput.safeParse(body);
+      if (!safeParse.success) {
+        return httpError(
+          next,
+          new Error(ResponseMessage.INVALID_INPUT),
+          req,
+          StatusCodes.ERROR.CLIENT_ERROR.BAD_REQUEST
+        );
+      }
+
+      const { shapes, roomId } = safeParse.data;
+
+      const room = await updateRoomShapes({
+        roomId: roomId,
+        shapes: shapes,
+        userId: String(user?.userId),
+      });
+
+      return httpResponse(
+        req,
+        res,
+        StatusCodes.SUCCESS.CREATED,
+        "Room Updated",
+        {
+          room: room,
+        }
+      );
+    }
+  ),
+
+  getRoomById: asyncErrorHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+      const { params, user } = req as AuthenticatedRequest;
+
+      const safeParse = GetRoomShapesInput.safeParse(params);
+      if (!safeParse.success) {
+        return httpError(
+          next,
+          new Error(ResponseMessage.INVALID_INPUT),
+          req,
+          StatusCodes.ERROR.CLIENT_ERROR.BAD_REQUEST
+        );
+      }
+
+      const { boardId } = safeParse.data;
+
+      const room = await getRoomById({
+        roomId: boardId,
+        userId: String(user?.userId),
+      });
+
+      if (!room) {
+        return httpError(
+          next,
+          new Error(ResponseMessage.NOT_FOUND),
+          req,
+          StatusCodes.ERROR.CLIENT_ERROR.NOT_FOUND
+        );
+      }
+
+      return httpResponse(req, res, StatusCodes.SUCCESS.OK, "Room Fetched", {
+        room: room,
+      });
+    }
+  ),
+};
---
+++ apps/backend/src/routers/roomRouter.ts
@@ -0,0 +1,14 @@
+import roomController from "@/controllers/roomController";
+import { Router } from "express";
+
+const roomRouter: Router = Router();
+
+roomRouter.post("/create-room", roomController.createRoom);
+roomRouter.get("/get-rooms", roomController.getRooms);
+roomRouter.get("/get-room-shapes", roomController.getRoomShapes);
+roomRouter.delete("/delete-room", roomController.deleteRoom);
+roomRouter.put("/rename-room", roomController.renameRoom);
+roomRouter.put("/update-room-shapes", roomController.updateRoomShapes);
+roomRouter.get("/get-room/:roomId", roomController.getRoomById); 
+
+export default roomRouter;

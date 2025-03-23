89304eb431181e345f6576f84c037272398cf5b3
Mayank <mayanktiwari1212.mt@gmail.com>
2025-03-23 11:39:01 +0530

    fix: Delete route

--- apps/backend/src/routers/roomRouter.ts
+++ apps/backend/src/routers/roomRouter.ts
@@ -6,9 +6,9 @@ const roomRouter: Router = Router();
 roomRouter.post("/create-room", roomController.createRoom);
 roomRouter.get("/get-rooms", roomController.getRooms);
 roomRouter.get("/get-room-shapes", roomController.getRoomShapes);
-roomRouter.delete("/delete-room", roomController.deleteRoom);
+roomRouter.delete("/delete-room/:boardId", roomController.deleteRoom);
 roomRouter.put("/rename-room", roomController.renameRoom);
 roomRouter.put("/update-room-shapes", roomController.updateRoomShapes);
-roomRouter.get("/get-room/:roomId", roomController.getRoomById); 
+roomRouter.get("/get-room/:roomId", roomController.getRoomById);
 
 export default roomRouter;

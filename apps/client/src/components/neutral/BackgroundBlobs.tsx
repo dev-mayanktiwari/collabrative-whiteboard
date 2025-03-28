9280a50859d195265ffcd3f1af7961a68915856d
Mayank <mayanktiwari1212.mt@gmail.com>
2025-03-28 19:08:27 +0530

    feat: Added background blobas

---
+++ apps/client/src/components/neutral/BackgroundBlobs.tsx
@@ -0,0 +1,9 @@
+const BackgroundBlobs = () => (
+  <div className="absolute top-0 left-0 w-full h-full overflow-hidden">
+    <div className="absolute -top-20 -left-20 w-64 h-64 bg-yellow-300 border-4 border-black rounded-full opacity-50"></div>
+    <div className="absolute top-1/4 -right-32 w-96 h-96 bg-blue-200 border-4 border-black rounded-full opacity-50"></div>
+    <div className="absolute -bottom-20 -left-20 w-80 h-80 bg-pink-300 border-4 border-black rounded-full opacity-50"></div>
+  </div>
+);
+
+export default BackgroundBlobs;
